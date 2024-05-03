9deb0be6db9e4992f23b8342616a05a47eab6802
speter6501 <speter650@gmail.com>
2024-05-03 17:58:47 +0900

    #36 로그인 기능 추가

---
+++ src/main/java/music/domain/dto/User.java
@@ -0,0 +1,23 @@
+package music.domain.dto;
+
+public class User {
+    private String id;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
---
+++ src/main/java/music/ui/console/UserCommand.java
@@ -0,0 +1,30 @@
+package music.ui.console;
+
+public enum UserCommand {
+    SIGNUP("1","회원가입"),
+    LOGIN("2", "로그인");
+
+    private final String code;
+    private final String title;
+
+    UserCommand(String code, String title) {
+        this.code = code;
+        this.title = title;
+    }
+    public String getCode() {
+        return code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static UserCommand from(String code) {
+        for (UserCommand command : values()) {
+            if (command.code.equals(code)) {
+                return command;
+            }
+        }
+        throw new IllegalArgumentException("해당 " + code + "번 명령어는 찾을 수 없습니다");
+    }
+}
